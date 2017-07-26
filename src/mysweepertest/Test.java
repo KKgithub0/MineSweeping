1974a183a5e1a0682abab15efed8939d43be409d
Snowman <xukai_bx@163.com>
2017-07-26 08:07:40 +0800

    Add files via upload

---
+++ src/mysweepertest/Test.java
@@ -0,0 +1,12 @@
+package testpackage;
+
+import mysweeperframe.Mysweeperframe;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mysweeperframe ss = new Mysweeperframe();
+	}
+
+}
